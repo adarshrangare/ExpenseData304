512357cf2b97895e4fdaf675eb87f4e6ceb0516d
Adarsh Rangare <akrangare@gmail.com>
2025-02-05 02:35:00 +0530

    configured PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import withPWA from 'next-pwa'
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config: NextConfig = {
+  /* your existing config options here */
+}
+
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})(config)
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,29 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Expense Tracker 304",
   description: "Created by Adarsh",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: {
+    icon: [
+      { url: "/android-chrome-192x192.png", sizes: "192x192" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png" },
+    ],
+    shortcut: [{ url: "/android-chrome-192x192.png" }],
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Expense Tracker",
+  },
+  applicationName: "Expense Tracker 304",
+  formatDetection: {
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
